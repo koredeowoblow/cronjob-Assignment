9598e93e108a9b436a3dc3f3c33c9b8d2cc3dcb0
Owolabi shinaayomi <100287793+koredeowoblow@users.noreply.github.com>
2025-07-27 01:53:20 +0100

    route correction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Jobs\NotifyFansOfNewPost;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +20,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->app->singleton(NotifyFansOfNewPost::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,27 +10,12 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Public routes
 Route::post('/sign-up', [TestingController::class, 'signUp']);
 Route::post('/login', [TestingController::class, 'login']);
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user(); // OR Auth::user();
-});
-
-// Protected routes - requires Sanctum token
 Route::middleware('auth:sanctum')->group(function () {
-
-    // Follow and unfollow
     Route::post('/follow/{userId}', [FanController::class, 'follow']);
     Route::delete('/unfollow/{userId}', [FanController::class, 'unfollow']);
-
-    // Create post
     Route::post('/posts', [PostController::class, 'store']);
-
-    // Send notification
     Route::post('/send-notification', [TestingController::class, 'sendNotification']);
-
-    // Logout (optional)
     Route::post('/logout', [TestingController::class, 'logout']);
 });
